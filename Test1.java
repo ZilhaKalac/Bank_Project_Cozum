dbbb8b73676440c2ad2d3e549f6acb54d76f8e5d
ZilhaKalac <zilhakalac@gmail.com>
2025-09-16 21:36:49 +0300

    update

--- Test1.java
+++ Test1.java
@@ -3,5 +3,6 @@ package JavaProje06.Bank_Project_Cozum;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("ilk test yapıldı");
+        System.out.println("bu testte update yapılmıstır");
     }
 }
